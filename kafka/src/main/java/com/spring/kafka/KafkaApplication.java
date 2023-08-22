b8c049b576e31a95b4bc86307c3cc642474cb94a
ismail.sanan <s303501@studenti.polito.it>
2023-08-22 10:29:08 +0200

    command line runner removed

--- kafka/src/main/java/com/spring/kafka/KafkaApplication.java
+++ kafka/src/main/java/com/spring/kafka/KafkaApplication.java
@@ -1,14 +1,21 @@
 package com.spring.kafka;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api")
 public class KafkaApplication {
-
+		private final  KafkaTemplate<String, String> kafkaTemplate;
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaApplication.class, args);
 	}
@@ -19,13 +26,9 @@ public class KafkaApplication {
 	// kafka assigns each consumer a different partition. his consumer gorup will receive the massages in load-balancer
 	//producer json  kafka topic 3 partitions each partition to different consumer but same consumer group
 
-	@Bean
-	CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate){
-		return args -> {
-
-			kafkaTemplate.send("code" , "hellow kafka");
-
-
-		};
+	@GetMapping
+	public String ok(){
+		kafkaTemplate.send("code" , "hellow kafka");
+		return  "OK";
 	}
 }
